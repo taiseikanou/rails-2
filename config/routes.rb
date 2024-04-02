02ebfb561c46fcc989cb243f606c13bdcda742af
加納大成 <kanoutaisei@kanoutaiseinoMacBook-Air.local>
2024-04-02 15:58:54 +0900

    deviceの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'rooms#top'
   get 'users/account'
   get 'users/profile'
   get 'users/profile/edit', to:'users#profile_edit'
   patch 'users/profile/edit', to: 'users#profile_update'
-
   get 'rooms/own'
   resources :reservations
   resources :rooms
