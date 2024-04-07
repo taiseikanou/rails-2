aac745b2786cded2cb596c5450e5b87c5c6e4860
加納大成 <kanoutaisei@kanoutaiseinoMacBook-Air.local>
2024-04-07 16:52:58 +0900

    deviseの日本語化

--- Gemfile
+++ Gemfile
@@ -57,4 +57,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'image_processing', '~> 1.2'
 gem 'rails-i18n'
+gem 'devise-i18n'
 gem 'devise-i18n-views'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_05_065403) do
+ActiveRecord::Schema.define(version: 2024_04_02_090251) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 2024_04_05_065403) do
     t.datetime "check_in"
     t.datetime "check_out"
     t.integer "number_of_guests"
+    t.integer "user_id", null: false
     t.integer "room_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
     t.index ["room_id"], name: "index_reservations_on_room_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2024_04_05_065403) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,14 +67,16 @@ ActiveRecord::Schema.define(version: 2024_04_05_065403) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.text "introduction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reservations", "rooms"
+  add_foreign_key "reservations", "users"
 end
